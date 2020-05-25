1e2bacd62f0696777f666f60d096adf9ae59893e
Aminjon <aminmirzoev123@gmail.com>
2020-05-25 21:00:22 +0500

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HW25_20_05_2020.Models;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace HW25_20_05_2020.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string conString = "Data Source=MACHINE-PGO7H84;Initial Catalog=MobileStoreDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public IActionResult Index()
+        {
+            using (IDbConnection db = new SqlConnection(conString))
+            {
+
+                var personList = db.Query<Person>($"SELECT * FROM PERSON").ToList();
+                return View(personList);
+            }
+            
+        }
+
+       
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HW25_20_05_2020.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
